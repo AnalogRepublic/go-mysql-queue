5bb0c5ed7511b77b6ae83016c75ff26b301fe391
Craig Childs <childscraig17@gmail.com>
2018-08-03 10:44:07 +0100

    Define event table schema

--- msq/models.go
+++ msq/models.go
@@ -1,9 +1,17 @@
 package msq
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Event struct {
 	gorm.Model
-	UID       string `gorm:"type:varchar(255);primary_key"`
-	Namespace string `gorm:"type:varchar(255);index:namespace;not null"`
+	UID       string     `gorm:"type:varchar(255);primary_key"`
+	Namespace string     `gorm:"type:varchar(255);index:namespace;not null"`
+	Name      string     `gorm:"type:varchar(255);index:name"`
+	Payload   string     `gorm:"type:text"`
+	Retries   int        `gorm:"size:1;index:retries"`
+	CreatedAt *time.Time `gorm:"index:created_at;not null"`
 }
